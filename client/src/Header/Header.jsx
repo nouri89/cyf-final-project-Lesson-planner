506f84a2f06d86f5ae302daa898693a7987162ad
Patrick Chan <q_p18@hotmail.com>
2020-12-15 15:57:00 +0000

    Add CYF logo to Header component

---
+++ client/src/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CYF_Brand from "../img/cyf_brand.png";
+
+const Header = () => {
+  return <img className="logo" src={CYF_Brand} alt="CodeYourFuture logo" />
+};
+
+export default Header;
