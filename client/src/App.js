405a7489e8c77aca080b1fac5e2bf550902bad5e
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-12-15 12:54:07 +0000

    FakeLessons
    Fake Lessons json file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getMessage } from "./service";
 import logo from "./logo.svg";
+import UpcomingLessons from "./UpcomingLessons";
 
 export function App() {
 	const [message, setMessage] = useState("Loading...");
