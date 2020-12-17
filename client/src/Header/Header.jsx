60753738a3019d1966e6acde040bc1deefc646d9
Patrick Chan <q_p18@hotmail.com>
2020-12-17 05:39:05 +0000

    Change logo folder import path. Have component in Bootstrap grid to allign logo to the left

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import CYF_Brand from "../img/cyf_brand.png";
+import CYF_Brand from"../Images/cyf_brand.png";
 
 const Header = () => {
-  return <img className="logo" src={CYF_Brand} alt="CodeYourFuture logo" />
+  return (
+    <div className="container">
+      <div className="row">
+        <img className="logo pb-5" src={CYF_Brand} alt="CodeYourFuture logo" />
+      </div>
+    </div>
+  )
 };
 
 export default Header;
