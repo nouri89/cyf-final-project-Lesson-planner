a3658fa549252bc286ebf9bc9f2a81f19908dec0
Patrick Chan <q_p18@hotmail.com>
2020-12-17 06:05:13 +0000

    Add Footer component

--- client/src/LessonPage.jsx
+++ client/src/LessonPage.jsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header/Header";
 import LessonPage_Info from "./LessonPage/LessonPage_Info";
 import LessonPage_Volunteers from "./LessonPage/LessonPage_Volunteers";
+import Footer from "./Footer/Footer";
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
             <Header />
             <LessonPage_Info />
             <LessonPage_Volunteers />
+            <Footer />
         </div>
     );
 }
