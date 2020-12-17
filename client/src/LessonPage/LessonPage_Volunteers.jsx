652ba3c9553db85aea6caf177f31258c93156770
Patrick Chan <q_p18@hotmail.com>
2020-12-17 05:44:31 +0000

    Style table border. Have table in grid - each part occupies six columns

--- client/src/LessonPage/LessonPage_Volunteers.jsx
+++ client/src/LessonPage/LessonPage_Volunteers.jsx
@@ -3,40 +3,40 @@ import React from "react";
 const LessonPage_Volunteers = () => {
   return (
     <div>
-      <h2 className="text-center">People Volunteering:</h2>
-      <table className="table table-striped table-bordered table-hover table-condensed">
-        <tbody>
-          <tr className="table-danger">
-            <td>Claire Bickley</td>
-            <td>Regional Manager</td>
+      <h2 className="text-center">Volunteers at this Lesson:</h2>
+      <table className="container table table-striped table-hover table-condensed">
+        <tbody className="row d-flex justify-content-center">
+          <tr className="row col-md-6 table-danger border border-danger rounded bg-danger">
+            <td className="col-md-6">Claire Bickley</td>
+            <td className="col-md-6">Regional Manager</td>
           </tr>
-          <tr className="table-warning">
-            <td>Emile Pafford-Wray</td>
-            <td>Lead Teacher</td>
+          <tr className="row col-md-6 table-primary border border-primary rounded bg-primary">
+            <td className="col-md-6">Emile Pafford-Wray</td>
+            <td className="col-md-6">Lead Teacher</td>
           </tr>
-          <tr className="table-success">
-            <td>Nadir</td>
-            <td>Teaching Assistant</td>
+          <tr className="row col-md-6 table-success border border-success rounded bg-success">
+            <td className="col-md-6">Nadir</td>
+            <td className="col-md-6">Teaching Assistant</td>
           </tr>
-          <tr className="table-success">
-            <td>Nick Holdsworth</td>
-            <td>Teaching Assistant</td>
+          <tr className="row col-md-6 table-success border border-success rounded bg-success">
+            <td className="col-md-6">Nick Holdsworth</td>
+            <td className="col-md-6">Teaching Assistant</td>
           </tr>
-          <tr className="table-success">
-            <td>Wahab Rahman</td>
-            <td>Teaching Assistant</td>
+          <tr className="row col-md-6 table-success border border-success rounded bg-success">
+            <td className="col-md-6">Wahab Rahman</td>
+            <td className="col-md-6">Teaching Assistant</td>
           </tr>
-          <tr className="table-info">
-            <td>Edgar Endiulis</td>
-            <td>Personal Deveopment Rep</td>
+          <tr className="row col-md-6 table-warning border border-warning rounded bg-warning">
+            <td className="col-md-6">Edgar Endiulis</td>
+            <td className="col-md-6">Personal Deveopment Rep</td>
           </tr>
-          <tr className="table-primary">
-            <td>Shukri</td>
-            <td>Zoom Admin</td>
+          <tr className="row col-md-6 table-info border border-info rounded bg-info">
+            <td className="col-md-6">Shukri</td>
+            <td className="col-md-6">Zoom Admin</td>
           </tr>
-          <tr className="table-secondary">
-            <td>Atanas Kozarev</td>
-            <td>Host</td>
+          <tr className="row col-md-6 table-secondary border border-secondary rounded bg-secondary">
+            <td className="col-md-6">Atanas Kozarev</td>
+            <td className="col-md-6">Host</td>
           </tr>
         </tbody>
       </table>
