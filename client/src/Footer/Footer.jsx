8b2230f1c91c4e795fc7e4d2d57a3df119985ed3
Patrick Chan <q_p18@hotmail.com>
2020-12-17 06:02:59 +0000

    Add Footer

---
+++ client/src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© All rights reserved |  Registered charity 1174929</p>
+      <div className="footer"></div>
+    </footer>
+  )
+};
+
+export default Footer;
