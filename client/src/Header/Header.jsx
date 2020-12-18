2b6b68ec35f1b8da7d784b173dcfc6ae9cdb67ce
Patrick Chan <q_p18@hotmail.com>
2020-12-18 17:56:39 +0000

    Delete jsx files because they are duplicates of js files

--- client/src/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CYF_Brand from "../img/cyf_brand.png";
-
-const Header = () => {
-  return <img className="logo" src={CYF_Brand} alt="CodeYourFuture logo" />
-};
-
-export default Header;
