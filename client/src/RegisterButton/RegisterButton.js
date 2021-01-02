6362adada382dd0d05b3b4562f479bb357357a5d
Ali Haider <alihaiderx736@gmail.com>
2021-01-02 19:43:12 +0000

    -register button component created.

---
+++ client/src/RegisterButton/RegisterButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RegisterButton = () => {
+  return (
+  
+        <div >
+          <a href="/">
+        <strong className="nav_Home nav_register">Register</strong>
+        </a>
+      </div>
+  )
+};
+export default RegisterButton;
