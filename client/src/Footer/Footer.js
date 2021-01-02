6714a0ed4e4e310ce8bce10f08e7e5444585cd9e
Ali Haider <alihaiderx736@gmail.com>
2021-01-02 19:36:27 +0000

    -footer recreated and designed. -social media icons added. -css corrected.

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>© All rights reserved |  Registered charity 1174929</p>
-      <div className="footer"></div>
-    </footer>
+      <div className="footer"> 
+     <span>© All rights reserved |  Registered charity </span>  
+    <a target="_blank"  href="http://apps.charitycommission.gov.uk/Showcharity/RegisterOfCharities/CharityFramework.aspx?RegisteredCharityNumber=1174929&amp;SubsidiaryNumber=0">1174929</a>
+    <a target="_blank"  href="https://www.facebook.com/codeyourfuture.io">    <i class="fa fa-facebook-official"></i></a>
+    <a target="_blank"  href="https://twitter.com/CodeYourFuture_">    <i class="fa fa-twitter"></i></a>
+    <a target="_blank"  href="https://www.linkedin.com/company/codeyourfuture">    <i class="fa fa-linkedin-square"></i></a>
+    <a target="_blank"  href="mailto:contact@codeyourfuture.io">    <i class="fa fa-envelope" ></i></a>
+    
+      </div>
+
   )
 };
 
