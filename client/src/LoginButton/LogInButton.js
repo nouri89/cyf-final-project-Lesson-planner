a655a1a70354ace225b8f6d4e265ce824089427a
Ali Haider <alihaiderx736@gmail.com>
2021-01-02 19:41:27 +0000

    -login button component created.

---
+++ client/src/LoginButton/LogInButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LogInButton = () => {
+  return (
+  
+        <div  >
+          <a href="/">
+        <strong className="nav_Home nav_logIn">Log In</strong>
+        </a>
+      </div>
+  )
+};
+export default LogInButton;
