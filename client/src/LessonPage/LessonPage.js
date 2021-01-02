b9e3b258b5fed685384cb545109a8254e57e6b5d
Ali Haider <alihaiderx736@gmail.com>
2021-01-02 19:41:07 +0000

    -Available roles component added to the lesson page.

--- client/src/LessonPage/LessonPage.js
+++ client/src/LessonPage/LessonPage.js
@@ -7,14 +7,15 @@ import Header from "../Header/Header";
 import LessonPage_Info from "./LessonPage_Info";
 import LessonPage_Volunteers from "./LessonPage_Volunteers";
 import Footer from "../Footer/Footer";
+import AvailableRoles from "./AvailableRoles";
 
 
 const LessonPage = ({lessonId}) => {
     return (
         <div className="App">
             <Header />
-            <h1>Lesson id: {lessonId}</h1>
             <LessonPage_Info />
+            <AvailableRoles/>
             <LessonPage_Volunteers />
             <Footer />
         </div>
