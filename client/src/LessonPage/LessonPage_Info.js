d56caa6f28d7b217c3345ef1b34970b6a3b5fb9e
Ali Haider <alihaiderx736@gmail.com>
2021-01-04 04:34:55 +0000

    - date added to volunteers info component

--- client/src/LessonPage/LessonPage_Info.js
+++ client/src/LessonPage/LessonPage_Info.js
@@ -8,11 +8,12 @@ const LessonPage_Info = () => {
         <div className="col mx-2 my-2">
         <li><strong>Module: </strong><span>HTML - CSS</span></li>
         
-        <li><strong>Time: </strong><span>11:00 - 17:00</span></li>
+        <li><strong>Date: </strong><span>12-01-2020</span></li>
         <li><strong>Material: </strong><span><a href="https://syllabus.codeyourfuture.io/html-css/week-1/lesson" target="_blank">https://syllabus.codeyourfuture.io/html-css/week-1/lesson</a></span></li>
         </div>
         <div className="col mx-2 my-2">
         <li><strong>Lead Teacher: </strong><span>Emile Pafford-Wray</span></li>
+        <li><strong>Time: </strong><span>11:00 - 17:00</span></li>
         <li><strong>Lesson: </strong><span>Week 1</span></li>
      </div>
       </ul>
